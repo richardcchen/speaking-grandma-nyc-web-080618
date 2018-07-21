d730a2a1e262acfd79770400c43756fe8eba64ca
Richard Chen <richard.c.chen@gmail.com>
2018-07-21 13:40:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,10 @@ def speak_to_grandma(phrase)
   if (phrase == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   end
-  
+  elsif (phrase == phrase.upcase)
+    return "NO, NOT SINCE 1938!"
+  end
+else
+  return "HUH?! SPEAK UP, SONNY!"
+end
 end
